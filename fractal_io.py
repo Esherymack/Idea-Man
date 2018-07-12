b74d9b996d50b542cb0a3794c76501406ea7a1ac
Madison Tibbett <Esherymack@users.noreply.github.com>
2018-07-11 22:39:37 -0400

    Update fractal_io.py

--- fractal_io.py
+++ fractal_io.py
@@ -3,8 +3,14 @@ from os.path import isfile, join
 import random
 
 VAR_DIR = './variation_styles'
+TYPE_DIR = './fractal_types'
 
 def get_variation_style():
     with open(VAR_DIR) as vd:
         var_list = list(filter(lambda x: x, vd.read().split('\n')))
     return random.choice(var_list)
+
+def get_fractal_type():
+       with open(TYPE_DIR) as td:
+            type_list = list(filter(lambda x: x, td.read().split('\n')))
+       return random.choice(type_list)
